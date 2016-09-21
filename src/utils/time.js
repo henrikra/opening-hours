ea62bbb6ab8589cfeb06990fd7b187a63ac9472d
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-09-21 22:28:02 +0300

    Add tests and implementation for secondsTo12HourClock

---
+++ src/utils/time.js
@@ -0,0 +1,27 @@
+import _ from 'lodash';
+
+export function secondsTo12HourClock(seconds) {
+  const middayStarts = seconds >= 43200;
+  const period = middayStarts ? 'pm' : 'am';
+  
+  const fullHours = _.floor(seconds / 3600);
+  let leftOverSeconds = seconds % 3600;
+  
+  let fullMinutes;
+  if (leftOverSeconds) {
+    fullMinutes = _.floor(leftOverSeconds / 60);
+    leftOverSeconds = leftOverSeconds % 60;
+  }
+
+  let finalOutput = '';
+
+  finalOutput += `${fullHours >= 12 ? fullHours - 12 : fullHours}`;
+  if (_.isNumber(fullMinutes)) {
+    finalOutput += `.${fullMinutes < 10 ? '0' : ''}${fullMinutes}`;
+  }
+  if (leftOverSeconds) {
+    finalOutput += `:${leftOverSeconds < 10 ? '0' : ''}${leftOverSeconds}`;
+  }
+
+  return `${finalOutput} ${period}`;
+}
---
+++ test/utils/time-test.js
@@ -0,0 +1,33 @@
+import { expect } from 'chai';
+
+import { secondsTo12HourClock } from '../../src/utils/time';
+
+describe('secondsTo12HourClock', () => {
+  it('returns only hours when even hours', () => {
+    expect(secondsTo12HourClock(32400)).to.equal('9 am');
+  });
+
+  it('returns hours and minutes when even minutes', () => {
+    expect(secondsTo12HourClock(37800)).to.equal('10.30 am');
+  });
+
+  it('return hours, minutes and seconds on uneven minutes', () => {
+    expect(secondsTo12HourClock(37801)).to.equal('10.30:01 am');
+  });
+
+  it('formats minimum value', () => {
+    expect(secondsTo12HourClock(0)).to.equal('0 am');
+  });
+
+  it('formats one second', () => {
+    expect(secondsTo12HourClock(1)).to.equal('0.00:01 am');
+  });
+
+  it('formats maximum value', () => {
+    expect(secondsTo12HourClock(86399)).to.equal('11.59:59 pm');
+  });
+
+  it('formats when midday starts', () => {
+    expect(secondsTo12HourClock(43200)).to.equal('0 pm');
+  });
+});
