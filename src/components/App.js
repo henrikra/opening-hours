976cbcc5cea500a1b3eca25ac725afa5356c5a56
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-09-22 01:02:27 +0300

    Print opening hours to front end

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
 import openingHoursMock from '../mock-data/openingHours.json';
-import { openingHoursForWeek } from '../utils/renderOpeningHours';
+import { simpleOpeningHours } from '../utils/renderOpeningHours';
 
 require('../../style/style.css');
 
 export default class App extends Component {
   render() {
-    const openingHoursForWeek = openingHoursForWeek(openingHoursMock);
+    const openingHoursForWeek = simpleOpeningHours(openingHoursMock);
     return (
-      <ul>
-        {openingHoursForWeek.map(weekdayOpeningHours => <li>{weekdayOpeningHours}</li>)}
-      </ul>
+      <pre>
+        {openingHoursForWeek}
+      </pre>
     );
   }
 }
